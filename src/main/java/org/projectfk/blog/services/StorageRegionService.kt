d99470b6e71c707a07933bbb0c9e5e75b4576eea
wusatosi <wusatosi@sina.com>
2018-10-05 23:59:30 +0800

    Added AvatarService & StorageRegionService (working on)

---
+++ src/main/java/org/projectfk/blog/services/StorageRegionService.kt
@@ -0,0 +1,24 @@
+package org.projectfk.blog.services
+
+import org.projectfk.blog.common.RandomStringGenerator
+import org.projectfk.blog.data.StorageRegion
+import org.projectfk.blog.data.StorageRegionRepo
+import org.projectfk.blog.data.User
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class StorageRegionService {
+
+    @Autowired
+    private lateinit var regionRepo: StorageRegionRepo
+
+    fun requestNewRegion(
+            user: User,
+            name: String = RandomStringGenerator.getRandom(5)
+    ): StorageRegion {
+        val region = StorageRegion(user, name)
+        return regionRepo.save(region)
+    }
+
+}
